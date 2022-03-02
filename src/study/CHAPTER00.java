0dfd7e479a88a69a3bdd51e66ecfa70aa5283398
SojeongYoony <y940425@gmail.com>
2022-03-02 16:22:11 +0900

    [2022-03-02]modify]

---
+++ src/study/CHAPTER00.java
@@ -0,0 +1,30 @@
+package study;
+
+public class CHAPTER00 {
+
+	public static void main(String[] args) {
+
+	/* 
+	 		[Git & Git Hub]
+	 	1. Git 이란?
+	 	   - 분산 버전관리 시스템(Distribute Version Control Systems - DVCS)
+	 	   - 컴퓨터 파일의 변경사항을 추적하고 여러명의 사용자들 간에 파일에 대한 작업을 조율하는데 사용
+	 	   - Git 공식 사이트 한글메뉴얼 : https://git-scm.com/book/ko/v2
+	 	   * 즉, 여러명의 개발자가 하나의 소프트웨어 개발 프로젝트에 참여할 때, 소스 코드를 관리하는데 사용
+	 	2. Git Hub이란?
+	 	3. Git 기본 용어
+	 		- Repository   : 저장소. 저장소는 히스토리, 태그, 소스의 가지치기 혹은 branch에 따라 버전을 저장.
+	 		- Working Tree : 저장소를 어느 한 시점을 바라보는 작업자의 현재 시점.
+	 		- Staging Area : 저장소에 커밋하기 전에 커밋을 준비하는 위치
+	 		- Commit 	   : 현재 변경된 작업 상태를 점검을 마치면 확정하고 저장소에 저장하는 작업
+	 		- Head 		   : 현재 작업중인 Branch를 가리킴
+	 		- Branch 	   : 가지 또는 분기점. 작업을 할때에 현재 상태를 복사하여 Branch에서 작업 한 후, 
+	 				   	     완전 하다 싶을 때 Merge 함
+	 		- Merge 	   : 다른 Branch의 내용을 현재 Branch로 가져와 합치는 작업을 의미한다.
+	 
+	 */
+		
+		
+	}
+
+}
